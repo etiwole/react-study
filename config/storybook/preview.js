5175b04d9c64ff979fb36544a0c847059b6e4689
Андрей Коногоров <a.konogorov@uniqom.ru>
2023-05-20 17:26:11 +0700

    downgrade storybook from 7 to 6 for loki

---
+++ config/storybook/preview.js
@@ -0,0 +1,17 @@
+import { addDecorator } from '@storybook/react';
+import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from '../../src/app/providers/ThemeProvider';
+import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
+
+export const parameters = {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
+    },
+};
+
+addDecorator(ThemeDecorator(Theme.LIGHT));
+addDecorator(RouterDecorator);
